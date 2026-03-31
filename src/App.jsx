641c1ca205c57f7988cb850dad4af80b8dd2fb33
chifat2003 <nmchifat2004@gmail.com>
2026-03-31 20:04:03 +0600

    Setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Setup Done</p>
+        <p className='btn flex justify-center mt-5 p-2'>Tailwind working</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
